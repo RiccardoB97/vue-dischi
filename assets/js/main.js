b30a2d9ae9249c115447b26c475e02bcf9cd522b
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-05-28 12:57:23 +0200

    Defined basic structure of HTML/CSS/JS

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#app',
+
+    data: {
+        albums: {},
+
+    },
+
+    methods: {},
+
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(response => {
+                this.albums = response.data.response;
+                console.log(this.response);
+            })
+    }
+})
